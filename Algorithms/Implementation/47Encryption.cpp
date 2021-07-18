497106325d3d240e97b96075456764c86dcf4647
Ajay Sehrawat <40054831+ajay-sehrawat@users.noreply.github.com>
2021-07-18 23:01:56 +0530

    Adding Encryption Solution(Was easy to implement)

---
+++ Algorithms/Implementation/47Encryption.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    int c = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            s[c] = s[i];
+            c++;
+        }
+    }
+    s[c] = '\0';
+
+    //Root, floor and ceil value
+    float v_root = sqrt(c);
+    int x_ceil = ceil(v_root);
+    int x_floor = floor(v_root);
+
+    //to check if row * column > length(after removing spaces) given constraint
+    if ((x_ceil * x_floor) < c)
+    {
+        x_floor++;
+    }
+    /*testin code
+    cout << x_floor << endl
+         << x_ceil << endl;*/
+
+    //Lets break and store in a 2D array
+    char str[x_floor][x_ceil];
+    int k = 0;
+    for (int i = 0; i < x_floor; i++)
+    {
+        for (int j = 0; j < x_ceil; j++)
+        {
+                str[i][j] = s[k];
+                k++;
+        }
+    }
+
+    //printing the encryption
+    for (int i = 0; i < x_ceil; i++)
+    {
+        for (int j = 0; j < x_floor; j++)
+        {
+            if ( str[j][i] != '\0')
+            cout << str[j][i];
+        }
+        cout <<" ";
+    }
+}
