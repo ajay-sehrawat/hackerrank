d18546141e6410fa34b39f47f758000f1cb51850
Ajay Sehrawat <40054831+ajay-sehrawat@users.noreply.github.com>
2021-07-21 17:35:10 +0530

    Adding solution on 21/07/2021

---
+++ Algorithms/Implementation/51Beautiful_Triplets.cpp
@@ -0,0 +1,33 @@
+#include <bits\stdc++.h>
+using namespace std;
+int main()
+{
+    int n, d;
+    cin>>n>>d;
+    int arr[n];
+    for( int i =0; i<n; i++ )
+    {
+        cin>>arr[i];
+    }
+    int count = 0;
+    int temp_d = 0;
+    for ( int i=0; i<n; i++ )
+    {
+        for ( int j=0; j<n; j++ )
+        {
+            if ( arr[j] - arr[i] == d )
+            {
+                for ( int k =j; k<n; k++ )
+                {
+                    if ( arr[k] - arr[j] == d )
+                    {
+                        count++;
+                        break; 
+                    }
+                       
+                }
+            }
+        }
+    }
+    cout<<count;
+}
